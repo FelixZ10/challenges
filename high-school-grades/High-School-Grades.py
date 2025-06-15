b10a661d0651aad324b56e84e8ba692b0a2be60f
FelixZ10 <heitorfelixcosta34@gmail.com>
2025-06-15 13:35:01 -0300

    initial work on high-school-grades

---
+++ high-school-grades/High-School-Grades.py
@@ -0,0 +1,20 @@
+# Write code below 💖
+
+question = int(input("Enter here your grade:"))
+
+grade = int(question)
+
+if grade == 9:
+  print('Freshman')
+
+elif grade == 10:
+  print('Sophomore')
+
+elif grade == 11:
+  print("Junior")
+
+elif grade == 12:
+  print('Senior')
+
+else:
+  print('TBD')
